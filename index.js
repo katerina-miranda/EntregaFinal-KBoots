c22c434540ae76d07cf6876b71ccafeac8869a2e
Katerina Miranda <mirandakaterina1@gmail.com>
2022-11-14 00:27:03 -0300

    objetos

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //Usuario y contraseña
 
-function pedirNombre(){
+function pedirNombre() {
   let nombre = prompt('Ingresá tu nombre');
   saluda(nombre);
 }
@@ -9,18 +9,20 @@ const saluda = (usuario) => {
   alert(`Bienvenida/o ${usuario}`);
 }
 
-function usuarioContrasena(){
+function usuarioContrasena() {
   let usuario = prompt('Ingresá tu usuario');
   while(usuario != usuario) {
     alert('Usuario incorrecto');
     usuario = prompt('Ingresá tu usuario');
     return usuario;
-  } 
+  }
+
   let contrasena = prompt('Ingresá una contraseña');
   while(contrasena != contrasena) {
     alert('Contraseña incorrecta');
     contrasena = prompt('Ingresá una contraseña');
-  }   
+  }
+
   alert(`Hola, ${usuario} bienvenida/o a K-SHOES`);
 }
 
@@ -30,66 +32,140 @@ usuarioContrasena();
 //Cargar productos
 
 class Producto {
-  constructor(nombre, id, precio, color, talle, stock) {
-    this.nombre = nombre;
-    this.id = id;
-    this.precio = precio;
-    this.color = color;
-    this.talle = talle;
-    this.stock = stock;
+  constructor(detalle) {
+    this.nombre = detalle.nombre;
+    this.id = detalle.id;
+    this.precio = detalle.precio;
+    this.color = detalle.color;
+    this.talle = detalle.talle;
+    this.stock = detalle.stock;
+    this.vendido = false;
   }
-  vender(cantidad) {
+
+  vender() {
     this.stock = this.stock - cantidad;
   }
 }
 
 //Inicializo arrays
 
-const bota = [];
-const texana = [];
-const borcego = [];
+const botas = [];
+const texanas = [];
+const borcegos = [];
 
 //Cargo el stock de productos
 
-bota.push(new Producto('Botas Leather', 1, '$22.550', 'Negro', '45', 30));
-bota.push(new Producto('Botas Louisiana', 2, '$14.850', 'Blanco', '45', 10));
-bota.push(new Producto('Botas Cinnamon', 3, '$18.260', 'Negro', '46', 40));
+botas.push(new Producto({
+  nombre: 'Botas Leather',
+  id: 1,
+  precio: '$22.550',
+  color: 'Negro',
+  talle: '45',
+  stock: 10
+}));
 
-texana.push(new Producto('Texanas Apola', 4, '$6.300', 'Blanco', '46', 15));
-texana.push(new Producto('Texanas Aria', 5, '$12.900', 'Negro', '46', 22));
-texana.push(new Producto('Texanas Shirp', 6, '$13.800', 'Blanco', '45', 30));
+botas.push(new Producto({
+  nombre: 'Botas Louisiana',
+  id: 2,
+  precio: '$14.850',
+  color: 'Blanco',
+  talle: '45',
+  stock: 15
+}));
 
-borcego.push(new Producto('Borcegos Beta', 7, '$9.999', 'Negro', '45', 21));
-borcego.push(new Producto('Borcegos Genesis', 8, '$15.480', 'Blanco', '46', 30));
-borcego.push(new Producto('Borcegos Marig', 9, '$15.950', 'Negro', '45', 30));
+botas.push(new Producto({
+  nombre: 'Botas Cinnamon',
+  id: 3,
+  precio: '$18.260',
+  color: 'Negro',
+  talle: '46',
+  stock: 14
+}));
 
-//Demuestro los productos 
+texanas.push(new Producto({
+  nombre: 'Texanas Apola',
+  id: 4,
+  precio: '$6.300',
+  color: 'Blanco',
+  talle: '46',
+  stock: 15
+}));
 
-for(const botas of bota) {
-  alert('ID (' + botas.id + ') - ' + botas.nombre + ' ' + botas.precio);
+texanas.push(new Producto({
+  nombre: 'Texanas Aria',
+  id: 5,
+  precio: '$12.900',
+  color: 'Negro',
+  talle: '46',
+  stock: 12
+}));
+
+texanas.push(new Producto({
+  nombre: 'Texanas Shirp',
+  id: 6,
+  precio: '$13.800',
+  color: 'Blanco',
+  talle: '45',
+  stock: 9
+}));
+
+borcegos.push(new Producto({
+  nombre: 'Borcegos Beta',
+  id: 7,
+  precio: '$9.999',
+  color: 'Negro',
+  talle: '45',
+  stock: 15
+}));
+
+borcegos.push(new Producto({
+  nombre: 'Borcegos Genesis',
+  id: 8,
+  precio: '$15.480',
+  color: 'Blanco',
+  talle: '46',
+  stock: 14
+}));
+
+borcegos.push(new Producto({
+  nombre: 'Borcegos Marig',
+  id: 9,
+  precio: '$15.950',
+  color: 'Negro',
+  talle: '45',
+  stock: 10
+}));
+
+//Demuestro los productos
+
+for(const bota of botas) {
+  alert('ID (' + bota.id + ') - ' + bota.nombre + ' ' + botas.precio);
 }
-for(const texanas of texana) {
-  alert('ID (' + texanas.id + ') - ' + texanas.nombre + ' ' + texanas.precio);
+for(const texana of texanas) {
+  alert('ID (' + texana.id + ') - ' + texana.nombre + ' ' + texana.precio);
 }
-for(const borcegos of borcego) {
-  alert('ID (' + borcegos.id + ') - ' + borcegos.nombre + ' ' + borcegos.precio);
+for(const borcego of borcegos) {
+  alert('ID (' + borcego.id + ') - ' + borcego.nombre + ' ' + borcego.precio);
 }
 
 //Solicito ID de los zapatos
 
 let botaSeleccionada = parseInt(prompt('Ingresá el ID de las botas que deseas comprar:'));
-const botasElegidas = bota.find(botas => botas.id === botaSeleccionada);
-const texanasElegidas = texana.find(texanas => texanas.id === botaSeleccionada);
-const borcegosElegidos = borcego.find(borcegos => borcegos.id === botaSeleccionada);
+const botasElegidas = botas.find(botas => botas.id === botaSeleccionada);
+const texanasElegidas = texanas.find(texanas => texanas.id === botaSeleccionada);
+const borcegosElegidos = borcegos.find(borcegos => borcegos.id === botaSeleccionada);
 
 if(botaSeleccionada <= 0) {
   alert('Ingresá un ID válido');
 } else if(botaSeleccionada <= 3) {
-  alert(`Seleccionaste ${botasElegidas.nombre} ${botasElegidas.precio} \nTocá aceptar para agregarlas a tu carrito`); 
+  alert(`Seleccionaste ${botasElegidas.nombre} ${botasElegidas.precio}
+  \nTocá aceptar para agregarlas a tu carrito`);
 } else if(botaSeleccionada <= 6) {
-  alert(`Seleccionaste ${texanasElegidas.nombre} ${texanasElegidas.precio} \nTocá aceptar para agregarlas a tu carrito`);
+  alert(`Seleccionaste ${texanasElegidas.nombre} ${texanasElegidas.precio}
+  \nTocá aceptar para agregarlas a tu carrito`);
 } else if(botaSeleccionada <= 9) {
-  alert(`Seleccionaste ${borcegosElegidos.nombre} ${borcegosElegidos.precio} \nTocá aceptar para agregarlos a tu carrito`);
+  alert(`Seleccionaste ${borcegosElegidos.nombre} ${borcegosElegidos.precio}
+  \nTocá aceptar para agregarlos a tu carrito`);
 } else {
   alert('Ingresá un ID válido');
 }
